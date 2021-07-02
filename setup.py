8b339853a0d61151d063450886c54e0b37cd9d01
Ziegfred Zorrilla <ziegfredzorrilla23@gmail.com>
2021-07-02 17:29:04 +0800

    change version release to 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="wikipedia-searcher",
-    version="0.0.1",
+    version="0.0.2",
     author="Ziegfred Zorrilla",
     author_email="ziegfredzorrilla23@gmail.com",
     description="A script that scrapes wikipedia",
