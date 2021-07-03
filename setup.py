1bc4b9016d5e27a5a7e03826220e9ebdb04c5c25
Ziegfred Zorrilla <ziegfredzorrilla23@gmail.com>
2021-07-03 19:24:32 +0800

    change version release to 0.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="wikipedia-searcher",
-    version="0.0.2",
+    version="0.0.3",
     author="Ziegfred Zorrilla",
     author_email="ziegfredzorrilla23@gmail.com",
     description="A script that scrapes wikipedia",
@@ -18,5 +18,16 @@ setuptools.setup(
     ],
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
+    install_requires = [
+        'beautifulsoup4==4.9.3',
+        'certifi==2021.5.30',
+        'chardet==4.0.0',
+        'idna==2.10',
+        'lxml==4.6.3',
+        'more-itertools==8.8.0',
+        'requests==2.25.1',
+        'soupsieve==2.2.1',
+        'urllib3==1.26.6'
+    ]
     python_requires=">=3.9",
 )
